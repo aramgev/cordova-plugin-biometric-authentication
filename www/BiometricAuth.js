3a74a2b4afc183da7ebe23fdca4ee19281749a55
aramg <aramg@PROGRAMMER7>
2020-01-22 10:46:14 +0400

    test alert

--- www/BiometricAuth.js
+++ www/BiometricAuth.js
@@ -1,5 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.testBiometric = function (arg0, success, error) {
-    exec(success, error, 'BiometricAuth', 'testBiometric', [arg0]);
+exports.analyze = function (arg0, success, error) {
+	alert('analyze');
+    exec(success, error, 'BiometricAuth', 'analyze', [arg0]);
 };
