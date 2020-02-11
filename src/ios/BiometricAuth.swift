e9d1f9948294d86dd02afb11ae164fb9f2597a6c
aramg <aramg@PROGRAMMER7>
2020-02-11 15:50:36 +0400

    fixess

--- src/ios/BiometricAuth.swift
+++ src/ios/BiometricAuth.swift
@@ -1,13 +1,14 @@
 /********* BiometricAuth.m Cordova Plugin Implementation *******/
+import Foundation
 
 @objc(BiometricAuth) class BiometricAuth : CDVPlugin {
-  @objc(BiometricAuth:) // Declare your function name.
+  @objc // Declare your function name.
   func analyze(command: CDVInvokedUrlCommand) { // write the function code.    /* 
      * Always assume that the plugin will fail.
      * Even if in this example, it can't.
      */    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");    // Set the plugin result to succeed.
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");    // Send the function result back to Cordova.
+    //var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");    // Set the plugin result to succeed.
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");    // Send the function result back to Cordova.
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
 }
